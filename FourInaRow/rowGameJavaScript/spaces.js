99e2e9f77e306fea48fd233bfd9b15859259ed1c
Tim La <email@example.com>
2019-01-10 17:51:23 -0500

    made chnages

--- FourInaRow/rowGameJavaScript/spaces.js
+++ FourInaRow/rowGameJavaScript/spaces.js
@@ -9,11 +9,14 @@ class Space {
     }
 
     drawSVGSpace() {
+        const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         svgSpace.setAttributeNS(null, "id", this.id);
         svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
         svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
         svgSpace.setAttributeNS(null, "r", this.radius - 8);
         svgSpace.setAttributeNS(null, "fill", "black");
         svgSpace.setAttributeNS(null, "stroke", "none");
+
+        document.getElementById("mask").appendChild(svgSpace);   
     }
 }
