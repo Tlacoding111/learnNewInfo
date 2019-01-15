dd5aa367286f4c2014989d975f9a901cde765f5f
Tim La <email@example.com>
2019-01-15 15:33:42 -0500

    made changes

---
+++ NodeBasics/nodeBasics.js
@@ -0,0 +1,10 @@
+//Lesson 1
+
+    //Console Log
+    console.log("Hello World:");
+
+    //Console Error
+    console.error("Ooops something went wrong");
+
+    //Console Print
+    console.dir({name:"Andrew", age:33});
