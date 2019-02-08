726917cca517be767cc707731a74e77d97ded122
Tim La <email@example.com>
2019-02-08 13:15:22 -0500

    made changes

--- JavaScript/beginningJS.js
+++ JavaScript/beginningJS.js
@@ -11,4 +11,15 @@
 // greet("John", "Smith");
 // greet("Mary", "Smith");
 
+// Day 4
 
+// let hour = 10;
+
+//     if (hour >=6 && hour < 12) {
+//         console.log("Good Morning");
+//     }
+//     else if (hour >= 12 && hour < 18) {
+//         console.log("Good Afternoon");
+//     }
+//     else
+//         console.log("Good Evening");
