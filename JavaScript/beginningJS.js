f115295c5e5a26df3832506228b3d432c7d7de3b
Tim La <email@example.com>
2019-02-10 16:34:31 -0500

    made changes

--- JavaScript/beginningJS.js
+++ JavaScript/beginningJS.js
@@ -29,3 +29,20 @@
 // for (let i = 0; i < 5; i++) {
 //     console.log("Hello World");
 // }
+
+// Day 6
+
+// function createCircle(radius) {
+//     return {
+//         radius, 
+//         draw() {
+//             console.log("draw");
+//         }
+//     };
+// }
+
+// const circle1 = createCircle(1);
+// console.log(circle1);
+
+// const circle2 = createCircle(2);
+// console.log(circle2);
